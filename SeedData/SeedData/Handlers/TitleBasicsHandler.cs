6d7bbc8deb7a6f5b0eaee99d7580710de9e9949d
Brezzor <oliver.bresson@gmail.com>
2025-10-08 15:47:09 +0200

    Mixed issue with dbcontext not having a reference to the titlebasic object.

--- SeedData/SeedData/Handlers/TitleBasicsHandler.cs
+++ SeedData/SeedData/Handlers/TitleBasicsHandler.cs
@@ -6,30 +6,38 @@ namespace SeedData.Handlers
     {
         public static void SeedTitleBasics(ImdbContext context, string titleBasicPath)
         {
-            var titleBasics = new List<TitleBasic>();
+			try
+			{
+                var titleBasics = new List<TitleBasic>();
 
-            // Seed TitleBasic
-            foreach (var line in File.ReadLines(titleBasicPath).Skip(1).Take(50000))
-            {
-                var columns = line.Split('\t');
-                var titleBasic = new TitleBasic
+                // Seed TitleBasic
+                foreach (var line in File.ReadLines(titleBasicPath).Skip(1).Take(50000))
                 {
-                    Tconst = columns[0],
-                    TitleType = columns[1],
-                    PrimaryTitle = columns[2],
-                    OriginalTitle = columns[3],
-                    IsAdult = sbyte.TryParse(columns[4], out var isAdult) ? isAdult : (sbyte)0,
-                    StartYear = int.TryParse(columns[5], out var startYear) ? startYear : default,
-                    EndYear = int.TryParse(columns[6], out var endYear) ? endYear : null,
-                    RuntimeMinutes = int.TryParse(columns[7], out var runtime) ? runtime : null
-                };
-                titleBasics.Add(titleBasic);
+                    var columns = line.Split('\t');
+                    var titleBasic = new TitleBasic
+                    {
+                        Tconst = columns[0],
+                        TitleType = columns[1],
+                        PrimaryTitle = columns[2],
+                        OriginalTitle = columns[3],
+                        IsAdult = sbyte.TryParse(columns[4], out var isAdult) ? isAdult : (sbyte)0,
+                        StartYear = int.TryParse(columns[5], out var startYear) ? startYear : default,
+                        EndYear = int.TryParse(columns[6], out var endYear) ? endYear : null,
+                        RuntimeMinutes = int.TryParse(columns[7], out var runtime) ? runtime : null
+                    };
+                    context.Attach(titleBasic);
+                    titleBasics.Add(titleBasic);
+                }
+
+                context.TitleBasics.AddRange(titleBasics);
+                context.SaveChanges();
+
+                Console.WriteLine("Seeded first 50,000 TitleBasic records.");
             }
-
-            context.TitleBasics.AddRange(titleBasics);
-            context.SaveChanges();
-
-            Console.WriteLine("Seeded first 50,000 TitleBasic records.");
+			catch (Exception ex)
+			{
+                Console.WriteLine(ex.InnerException?.Message);
+			}
         }
     }
 }
