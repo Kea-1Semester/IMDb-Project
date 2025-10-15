341e048de833cf0d644c7ab7a3a850a2964faebd
Brezzor <oliver.bresson@gmail.com>
2025-10-15 20:37:49 +0200

    Removed context from program for now.

--- SeedData/SeedData/Program.cs
+++ SeedData/SeedData/Program.cs
@@ -9,22 +9,7 @@ namespace SeedData
     {
         static void Main(string[] args)
         {
-            Env.TraversePath().Load();
-
-            var optionsBuilder = new DbContextOptionsBuilder<ImdbContext>()
-                .UseMySql(
-                    Env.GetString("ConnectionString"), 
-                    ServerVersion.AutoDetect(Env.GetString("ConnectionString"))
-                )
-                .LogTo(Console.WriteLine, LogLevel.Information)
-                .EnableDetailedErrors()
-                .EnableSensitiveDataLogging();
-
-            using (var context = new ImdbContext(optionsBuilder.Options))
-            {
-            }
-
-            Console.WriteLine("Data seeding completed.");
+            Console.WriteLine("Program Completed.");
         }
     }
 }
