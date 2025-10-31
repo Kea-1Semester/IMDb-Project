45378358f7a6fa69d447a95733d52f0ae5a52ac7
Rex77pro <smaurieka@gmail.com>
2025-10-31 15:01:46 +0100

    Bug(MongoDBSetting): Add MongoDBName
    Failed without adding a Name to the settings - The .env file need to set a name for the database too.

--- SeedData/SeedData/DbConnection/MongoDbSettings.cs
+++ SeedData/SeedData/DbConnection/MongoDbSettings.cs
@@ -6,11 +6,15 @@ namespace SeedData.DbConnection;
 
 public static class MongoDbSettings
 {
-    public static ImdbContextMongoDb MongoDbConnection(string connectionStr = "MongoDbConnectionStr")
+    public static ImdbContextMongoDb MongoDbConnection(
+        string connectionStr = "MongoDbConnectionStr",
+        string databaseNameKey = "MongoDbDatabase")
     {
         var mongoDbConnectionUri = Env.GetString(connectionStr);
+        var dbName = Env.GetString(databaseNameKey);
+
         var optionsBuilder = new DbContextOptionsBuilder<ImdbContextMongoDb>()
-            .UseMongoDB(mongoDbConnectionUri)
+            .UseMongoDB(mongoDbConnectionUri, dbName)
             .EnableDetailedErrors();
         return new ImdbContextMongoDb(optionsBuilder.Options);
     }
