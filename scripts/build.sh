d9aadf3d413ea2895706559aadc09b58dacadc7f
Brezzor <oliver.bresson@gmail.com>
2025-11-04 20:59:44 +0100

    Added SeedData project to build script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# Build frontend project
+echo "
+╔═══════════════════════════════════╗
+║         BUILDING PROJECTS         ║
+╚═══════════════════════════════════╝
+"
+
+echo "Navigating to frontend..."
+
+# Navigate to frontend project
 cd ./frontend || exit 1
 
 # Install dependencies
@@ -8,3 +16,11 @@ npm install
 
 # Run build script
 npm run build
+
+echo "Navigating to SeedData..."
+
+# Navigate to SeedData project
+cd ../SeedData || exit 1
+
+# Run dotnet build
+dotnet build
