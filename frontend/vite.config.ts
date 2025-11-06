e8fa9867b5aa59fcac220f2bae42e443a1916aca
Shero Shvan <sheroshvan@icloud.com>
2025-11-06 18:31:58 +0100

    adding .env package.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     port: 3000,
     host: '0.0.0.0',
-    open: true,
+    open: process.env.OPEN_BROWSER ? process.env.OPEN_BROWSER === 'true' : true,
     watch: {
       usePolling: true,
     },
