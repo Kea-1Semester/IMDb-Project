4b8cd86d096f4d2c843270d9145f51c45aa104d0
Rex77pro <smaurieka@gmail.com>
2025-11-10 13:32:01 +0100

    Feat(WIP: Neo Initializer and Mapper)

--- SeedData/EfCoreModelsLib/Models/Neo4J/Handler/Neo4jSchemaInitializer.cs
+++ SeedData/EfCoreModelsLib/Models/Neo4J/Handler/Neo4jSchemaInitializer.cs
@@ -9,20 +9,30 @@ namespace EfCoreModelsLib.models.Neo4J.Handler
     {
         public static async Task EnsureConstraintsAsync(string uri, string user, string password)
         {
-            var driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
-            await using var session = driver.AsyncSession();
+            await using var driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+            await using var session = driver.AsyncSession(o => o.WithDefaultAccessMode(AccessMode.Write));
 
-            const string constraint =
-                "CREATE CONSTRAINT attribute_id_Unique IF NOT EXISTS " +
-                "FOR (a:Attributes) REQUIRE a.AttributeId IS UNIQUE";
+            var statements = new[]
+            {
+                // Attributes
+                "CREATE CONSTRAINT attributes_id_unique IF NOT EXISTS FOR (a:Attributes) REQUIRE a.AttributeId IS UNIQUE",
+                "CREATE CONSTRAINT attributes_attribute_exists IF NOT EXISTS FOR (a:Attributes) REQUIRE a.Attribute IS NOT NULL",
+
+                // Eksempel på andre labels fra dit IMDB-domæne
+                "CREATE CONSTRAINT persons_id_unique IF NOT EXISTS FOR (p:Person) REQUIRE p.PersonId IS UNIQUE",
+                "CREATE CONSTRAINT titles_id_unique IF NOT EXISTS FOR (t:Title) REQUIRE t.TitleId IS UNIQUE",
+                "CREATE CONSTRAINT characters_id_unique IF NOT EXISTS FOR (c:Character) REQUIRE c.CharacterId IS UNIQUE",
+
+                // Relationsegenskaber (hvis du bruger properties på relationer)
+                // Fx hvis du fjerner Character-noden og gemmer rollen på relationen:
+                "CREATE CONSTRAINT acted_in_role_exists IF NOT EXISTS FOR ()-[r:PLAYED_IN]-() REQUIRE r.role IS NOT NULL"
+            };
 
             await session.ExecuteWriteAsync(async tx =>
             {
-                await tx.RunAsync(constraint);
+                foreach (var stmt in statements)
+                    await tx.RunAsync(stmt);
             });
-
-            await session.CloseAsync();
-            await driver.CloseAsync();
         }
     }
 }
--- SeedData/SeedData/Handlers/Neo4j/Neo4jMapper.cs
+++ SeedData/SeedData/Handlers/Neo4j/Neo4jMapper.cs
@@ -5,16 +5,68 @@ using Neo4j.Driver;
 using EfCoreModelsLib.models.Neo4J.Neo4JModels;
 using EfCoreModelsLib.models.Neo4J.Handler;
 using DotNetEnv;
+using MongoDB.Driver.Core.Misc;
+using EfCoreModelsLib.Models.Neo4J.Neo4JModels;
 
-namespace SeedData.Handlers.Neo4j;
- {
+namespace SeedData.Handlers.Neo4j
+{
     public static class Neo4jMapper
     {
         public static async Task MigrateToNeo4j(
-            IEnumerable<Attribute> source,
+            IEnumerable<AttributesEntity> source,
             int batchSize = 1000)
         {
-            
+            var uri = Env.GetString("NEO4J_URI");
+            var user = Env.GetString("NEO4J_USER");
+            var pass = Env.GetString("NEO4J_PASSWORD");
+
+            await Neo4jSchemaInitializer.EnsureConstraintsAsync(uri, user, pass);
+
+            var driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, pass));
+            await using var session = driver.AsyncSession();
+
+            var buffer = new List<AttributesEntity>(batchSize);
+
+            foreach (var attribute in source)
+            {
+                buffer.Add(attribute);
+
+                if (buffer.Count >= batchSize)
+                {
+                    await UpsertBatch(session, buffer);
+                    buffer.Clear();
+                }
+            }
+
+            if (buffer.Count > 0)
+            {
+                await UpsertBatch(session, buffer);
+            }
+
+            await session.CloseAsync();
+            await driver.CloseAsync();
+        }
+
+        public static async Task UpsertBatch(IAsyncSession session, List<AttributesEntity> attributes)
+        {
+            const string cypher = @"
+            UNWIND $rows AS row
+            MERGE (a:Attributes { AttributeId: row.AttributeId })
+            SET a.Attribute = row.Attribute";
+
+            var param = new
+            {
+                rows = attributes.ConvertAll(a => new
+                {
+                    AttributeId = a.AttributeId.ToString(),
+                    Attribute   = a.Attribute
+                })
+            };
+
+            await session.ExecuteWriteAsync(async tx =>
+            {
+                await tx.RunAsync(cypher, param);
+            });
         }
     }
- }
+}
