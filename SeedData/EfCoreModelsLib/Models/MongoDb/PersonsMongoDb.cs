59095594cd59fd14c035fc914a5be72c7b1b5873
Shero Shvan <103439939+Rasho-dk@users.noreply.github.com>
2025-11-12 00:25:53 +0100

    implement PersonValidator schema, that valdiate the schema in mongodb… (#42)

--- SeedData/EfCoreModelsLib/Models/MongoDb/PersonsMongoDb.cs
+++ SeedData/EfCoreModelsLib/Models/MongoDb/PersonsMongoDb.cs
@@ -1,22 +1,26 @@
 ﻿using EfCoreModelsLib.Models.MongoDb.SupportClasses;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace EfCoreModelsLib.Models.MongoDb
 {
     public class PersonsMongoDb
     {
-        [BsonId]
-        public Guid Guid { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+        [BsonElement("personId")]
+        [BsonRepresentation(BsonType.String)]
+        public Guid Guid { get; init; }
         [BsonElement("name")]
-        public string Name { get; set; } = null!;
+        public string Name { get; init; } = null!;
         [BsonElement("birthYear")]
-        public int BirthYear { get; set; }
+        public int BirthYear { get; init; }
         [BsonElement("endYear")]
-        public int? EndYear { get; set; }
+        public int? EndYear { get; init; }
         [BsonElement("professions")]
-        public List<string> Professions { get; set; } = null!;
+        public List<string> Professions { get; init; } = null!;
         [BsonElement("knownForTitles")]
-        public List<KnownForTitle> KnownFor { get; set; } = null!;
+        public List<KnownForTitle> KnownFor { get; init; } = null!;
 
     }
 }
