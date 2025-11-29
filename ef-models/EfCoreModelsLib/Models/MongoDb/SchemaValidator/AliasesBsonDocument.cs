0a3ca99e38b64cf16e42c999d368ed20f97359cd
Brezzor <oliver.bresson@gmail.com>
2025-11-30 00:09:44 +0100

    Added const's to shema validators: sonarqube

--- ef-models/EfCoreModelsLib/Models/MongoDb/SchemaValidator/AliasesBsonDocument.cs
+++ ef-models/EfCoreModelsLib/Models/MongoDb/SchemaValidator/AliasesBsonDocument.cs
@@ -6,35 +6,38 @@ public abstract class AliasesBsonDocument
 {
     public static BsonDocument GetSchema()
     {
+        const string bsonType = "bsonType";
+        const string stringType = "string";
+
         return new BsonDocument
         {
-            { "bsonType", "array" },
+            { bsonType, "array" },
             {
                 "items", new BsonDocument
                 {
-                    { "bsonType", "object" },
+                    { bsonType, "object" },
                     { "required", new BsonArray { "id", "title" } },
                     { "additionalProperties", false },
                     {
                         "properties", new BsonDocument
                         {
-                            { "id", new BsonDocument("bsonType", new BsonArray { "binData", "string" }) },
-                            { "region", new BsonDocument("bsonType", new BsonArray { "string", "null" }) },
-                            { "title", new BsonDocument("bsonType", "string") },
-                            { "language", new BsonDocument("bsonType", new BsonArray { "string", "null" }) },
-                            { "isOriginalTitle", new BsonDocument("bsonType", new BsonArray { "bool", "null" }) },
+                            { "id", new BsonDocument(bsonType, new BsonArray { "binData", stringType }) },
+                            { "region", new BsonDocument(bsonType, new BsonArray { stringType, "null" }) },
+                            { "title", new BsonDocument(bsonType, stringType) },
+                            { "language", new BsonDocument(bsonType, new BsonArray { stringType, "null" }) },
+                            { "isOriginalTitle", new BsonDocument(bsonType, new BsonArray { "bool", "null" }) },
                             {
                                 "types", new BsonDocument
                                 {
-                                    { "bsonType", "array" },
-                                    { "items", new BsonDocument("bsonType", new BsonArray { "string", "null" }) }
+                                    { bsonType, "array" },
+                                    { "items", new BsonDocument(bsonType, new BsonArray { stringType, "null" }) }
                                 }
                             },
                             {
                                 "attributes", new BsonDocument
                                 {
-                                    { "bsonType", "array" },
-                                    { "items", new BsonDocument("bsonType", new BsonArray { "string", "null" }) }
+                                    { bsonType, "array" },
+                                    { "items", new BsonDocument(bsonType, new BsonArray { stringType, "null" }) }
                                 }
                             }
                         }
