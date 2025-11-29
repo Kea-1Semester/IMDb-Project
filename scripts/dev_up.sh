b80f957970c38ed7ff87cdce1bdf0f9f7d54a1c8
Brezzor <oliver.bresson@gmail.com>
2025-11-30 00:30:26 +0100

    cleaned up scripts

--- scripts/dev_up.sh
+++ scripts/dev_up.sh
@@ -6,10 +6,5 @@ echo "
 ╚═══════════════════════════════════╝
 "
 
-echo "Navigating to root..."
-
-# Navigate to project root
-cd .. || exit 1
-
 # Start Docker containers in development mode
 docker-compose -f docker-compose.dev.yml up -d
