75091a2d83be3e740a46a07473a83ace6ad2a395
Shero Shvan <sheroshvan@icloud.com>
2025-12-02 07:40:44 +0100

    adding authorized data annotation for optional testing #61.

--- backend/GraphQL/Types/TitlesQuery.cs
+++ backend/GraphQL/Types/TitlesQuery.cs
@@ -12,7 +12,7 @@ public static class TitlesQuery
     [UseProjection]
     [UseFiltering]
     [UseSorting]
-    [Authorize(AuthPolicy.ReadPolicies)]
+    //[Authorize(AuthPolicy.ReadPolicies)] // optional for testing without auth
     public static IQueryable<Titles> GetTitles([Service] ITitlesService titlesService)
     {
         return titlesService.GetTitles();
