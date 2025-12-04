94c8713e103247056030da1d5288cf79a1c3d148
Brezzor <oliver.bresson@gmail.com>
2025-12-04 20:31:27 +0100

    Added Error of type ValidationException handler for MySqlPersons mutations.

--- backend/GraphQL/Types/MySql/Mutations/MysqlPersonsMutation.cs
+++ backend/GraphQL/Types/MySql/Mutations/MysqlPersonsMutation.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using EfCoreModelsLib.DTO;
 using EfCoreModelsLib.Models.Mysql;
 using GraphQL.Auth0;
@@ -9,16 +10,19 @@ namespace GraphQL.Types.Mysql.Mutations;
 [MutationType]
 public static class MysqlPersonsMutation
 {
+    [Error(typeof(ValidationException))]
     public static async Task<Persons> CreateMysqlPerson(PersonsDto person, [Service] IMysqlPersonsService personsService)
     {
         return await personsService.CreateMysqlPerson(person);
     }
 
+    [Error(typeof(ValidationException))]
     public static async Task<Persons> UpdateMysqlPerson(Guid id, PersonsDto person, [Service] IMysqlPersonsService personsService)
     {
         return await personsService.UpdateMysqlPerson(person, id);
     }
 
+    [Error(typeof(ValidationException))]
     public static async Task<Persons> DeleteMysqlPerson(Guid id, [Service] IMysqlPersonsService personsService)
     {
         return await personsService.DeleteMysqlPerson(id);
