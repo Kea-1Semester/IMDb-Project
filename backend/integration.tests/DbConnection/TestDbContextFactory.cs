ced42f5e2eca11a712cb8ecac99dc21aac48b107
Shero Shvan <sheroshvan@icloud.com>
2025-12-05 16:37:15 +0100

    Feat: add integration tests for MysqlTitlesRepo and related services Integration-test for ttiles and extranal api with pipeline Fixes #100

---
+++ backend/integration.tests/DbConnection/TestDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EfCoreModelsLib.Models.Mysql;
+using Microsoft.EntityFrameworkCore;
+
+namespace integration.tests.DbConnection
+{
+    public class TestDbContextFactory(IDbContextFactory<ImdbContext> dbContextFactory)
+        : IDbContextFactory<ImdbContext>
+    {
+        private readonly ImdbContext _options = dbContextFactory.CreateDbContext();
+
+        public ImdbContext CreateDbContext()
+        {
+            return _options;
+        }
+    }
+}
---
+++ backend/integration.tests/Mock/MockTitles.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EfCoreModelsLib.Models.Mysql;
+
+namespace integration.tests.Mock
+{
+    public static class MockTitles
+    {
+        private static List<Titles> _titlesList = new List<Titles>()
+        {
+            new()
+            {
+                TitleId = Guid.NewGuid(),
+                TitleType = "movie",
+                PrimaryTitle = "Inception",
+                OriginalTitle = "Inception",
+                IsAdult = false,
+                StartYear = 2010,
+                EndYear = null,
+                RuntimeMinutes = 148
+            },
+            new()
+            {
+                TitleId = Guid.NewGuid(),
+                TitleType = "series",
+                PrimaryTitle = "Breaking Bad",
+                OriginalTitle = "Breaking Bad",
+                IsAdult = false,
+                StartYear = 2008,
+                EndYear = 2013,
+                RuntimeMinutes = 49
+            }
+        };
+
+        public static List<Titles> GetMockTitles()
+        {
+            return _titlesList;
+        }
+    }
+
+   
+}
