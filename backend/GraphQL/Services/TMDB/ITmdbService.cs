361a887fd4e205eb739e0666d4c0948f93cac15e
Shero Shvan <sheroshvan@icloud.com>
2025-12-07 19:12:33 +0100

    Feat: implement ITmdbService and TmdbService for fetching movie details from TMDB API Integration-test for ttiles and extranal api with pipeline Fixes #100

---
+++ backend/GraphQL/Services/TMDB/ITmdbService.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GraphQL.Services.TMDB
+{
+    public interface ITmdbService
+    {
+        Task<List<MovieDetails>?> GetAll(int? newPage = null);
+    }
+}
+
+
+public record MovieDetails 
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+    [JsonPropertyName("original_title")]
+    public string Original_title { get; set; }
+    [JsonPropertyName("overview")]
+    public string Overview { get; set; }
+    [JsonPropertyName("poster_path")]
+    public string Poster_Path { get; set; }
+    [JsonPropertyName("release_date")]
+    public string Release_Date { get; set; }
+    [JsonPropertyName("vote_average")]
+    public double Vote_Average { get; set; }
+
+}
+
+public record MovieResponse
+{
+    [JsonPropertyName("results")]
+    public required List<MovieDetails> Results { get; set; }
+    [JsonPropertyName("page")]
+    public int Page { get; set; }
+
+}
---
+++ backend/GraphQL/Services/TMDB/TmdbService.cs
@@ -0,0 +1,30 @@
+﻿namespace GraphQL.Services.TMDB
+{
+    public class TmdbService : ITmdbService
+    {
+        private readonly HttpClient _httpClient;
+
+        public TmdbService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+
+        public async Task<List<MovieDetails>?> GetAll(int? newPage = null)
+        {
+            try
+            {
+                var result = await _httpClient.GetFromJsonAsync<MovieResponse>(
+                    newPage.HasValue ? $"discover/movie?page={newPage}" : "discover/movie"
+                );
+                return result?.Results;
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new Exception("Failed to fetch data from TMDB API", ex);
+            }
+        }
+
+
+    }
+}
