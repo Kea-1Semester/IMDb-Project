1792e0dc7669b357219a62623b13e5ad2be9e512
Shero Shvan <103439939+Rasho-dk@users.noreply.github.com>
2025-12-08 10:16:18 +0100

    Dev/integration test (#102)

---
+++ backend/GraphQL/Services/TMDB/TmdbService.cs
@@ -0,0 +1,28 @@
+﻿namespace GraphQL.Services.TMDB
+{
+    public class TmdbService : ITmdbService
+    {
+        private readonly HttpClient _httpClient;
+
+        public TmdbService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+
+        public async Task<List<MovieDetails>?> GetAll(int? newPage = null)
+        {
+            try
+            {
+                var result = await _httpClient.GetFromJsonAsync<MovieResponse>(
+                    newPage.HasValue ? $"discover/movie?page={newPage}" : "discover/movie"
+                );
+                return result?.Results;
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new HttpRequestException("Failed to fetch data from TMDB API", ex);
+            }
+        }
+    }
+}
