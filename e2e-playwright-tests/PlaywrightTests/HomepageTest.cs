292f2329d2f2803ad5bff8d261a8385ac51ea01d
Rex77pro <smaurieka@gmail.com>
2025-12-08 15:13:45 +0100

    Feat(Playwright + CICD)

---
+++ e2e-playwright-tests/PlaywrightTests/HomepageTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace E2E.Playwright.Tests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+[Category("E2ETest")]
+public class E2eTest : PageTest
+{
+    /*Run Test with Browser:
+    
+    dotnet test -- Playwright.BrowserName=chromium Playwright.LaunchOptions.Headless=false Playwright.LaunchOptions.Channel=msedge
+    */
+
+    /*For easy tests writing run:
+
+        powershell bin/Debug/net9.0/playwright.ps1 codegen
+        or
+        pwsh bin/Debug/net9.0/playwright.ps1 codegen
+    */
+
+    /*Test Template:
+    
+        [Test]
+        public async Task TestTemplate()
+        {
+            await Page.GotoAsync("/");
+            
+        }
+    */
+
+    [Test]
+    public async Task TestHomePage()
+    {
+        await Page.GotoAsync("http://localhost:3000/");
+        await Expect(Page.GetByRole(AriaRole.Heading)).ToContainTextAsync("Welcome to Sample0");
+    }
+}
