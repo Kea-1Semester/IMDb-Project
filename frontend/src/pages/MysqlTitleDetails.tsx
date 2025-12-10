6caa8056365dd10e837ff7e958de0ba84597d9fe
Brezzor <oliver.bresson@gmail.com>
2025-12-10 14:51:11 +0100

    Fixed issues with typescript errors and codegen

--- frontend/src/pages/MysqlTitleDetails.tsx
+++ frontend/src/pages/MysqlTitleDetails.tsx
@@ -1,14 +1,14 @@
 import { useQuery } from '@apollo/client/react';
 import { useNavigate, useParams } from 'react-router';
 import { Box, Button, Card, HStack, Text } from '@chakra-ui/react';
-import { gql } from '@/generated';
-import type { Titles } from '@/generated/types';
+import { graphql } from '@/generated/gql';
+import type { GetTitleQuery, Titles } from '@/generated/graphql';
 import QueryResult from '@/components/custom/QueryResult';
 import { RiArrowLeftLine } from 'react-icons/ri';
 
-const TITLE = gql(`
-  query GetTitle ($id: UUID) {
-    mysqlTitles(where: { titleId: { eq: $id } } ) {
+const TITLE = graphql(`
+  query GetTitle($id: UUID) {
+    mysqlTitles(where: { titleId: { eq: $id } }) {
       items {
         titleId
         primaryTitle
@@ -21,14 +21,15 @@ const TITLE = gql(`
         genresGenre {
           genreId
           genre
-        }       
+        }
       }
     }
-  }`);
+  }
+`);
 
 const MysqlTitleDetails = () => {
   const { id } = useParams<{ id: string }>();
-  const { loading, error, data } = useQuery(TITLE, { variables: { id: id ?? '' } });
+  const { loading, error, data } = useQuery<GetTitleQuery>(TITLE, { variables: { id: id ?? '' } });
 
   const navigate = useNavigate();
 
@@ -37,38 +38,41 @@ const MysqlTitleDetails = () => {
       <QueryResult loading={loading} error={error} data={data}>
         <Card.Root shadow={'sm'}>
           <Card.Body>
-            {data?.mysqlTitles?.items?.map((title: Titles, index) => (
-              <Box key={index}>
-                <HStack>
-                  <Text fontWeight={'bold'}>PrimaryTitle:</Text>
-                  <Text>{title.primaryTitle ?? '-'}</Text>
-                </HStack>
-                <HStack>
-                  <Text fontWeight={'bold'}>OriginalTitle:</Text>
-                  <Text>{title.originalTitle ?? '-'}</Text>
-                </HStack>
-                <HStack>
-                  <Text fontWeight={'bold'}>IsAdult:</Text>
-                  <Text>{title.isAdult ? 'Yes' : 'No'}</Text>
-                </HStack>
-                <HStack>
-                  <Text fontWeight={'bold'}>StartYear:</Text>
-                  <Text>{title.startYear ?? '-'}</Text>
-                </HStack>
-                <HStack>
-                  <Text fontWeight={'bold'}>EndYear:</Text>
-                  <Text>{title.endYear ?? '-'}</Text>
-                </HStack>
-                <HStack>
-                  <Text fontWeight={'bold'}>RuntimeMinutes:</Text>
-                  <Text>{title.runtimeMinutes ?? '-'}</Text>
-                </HStack>
-                <HStack>
-                  <Text fontWeight={'bold'}>Genres:</Text>
-                  <Text>{title.genresGenre?.map((genre) => genre?.genre).join(', ') ?? '-'}</Text>
-                </HStack>
-              </Box>
-            ))}
+            {data &&
+              data.mysqlTitles &&
+              data.mysqlTitles.items &&
+              data.mysqlTitles.items.map((title: Titles) => (
+                <Box key={title.titleId}>
+                  <HStack>
+                    <Text fontWeight={'bold'}>PrimaryTitle:</Text>
+                    <Text>{title.primaryTitle ?? '-'}</Text>
+                  </HStack>
+                  <HStack>
+                    <Text fontWeight={'bold'}>OriginalTitle:</Text>
+                    <Text>{title.originalTitle ?? '-'}</Text>
+                  </HStack>
+                  <HStack>
+                    <Text fontWeight={'bold'}>IsAdult:</Text>
+                    <Text>{title.isAdult ? 'Yes' : 'No'}</Text>
+                  </HStack>
+                  <HStack>
+                    <Text fontWeight={'bold'}>StartYear:</Text>
+                    <Text>{title.startYear ?? '-'}</Text>
+                  </HStack>
+                  <HStack>
+                    <Text fontWeight={'bold'}>EndYear:</Text>
+                    <Text>{title.endYear ?? '-'}</Text>
+                  </HStack>
+                  <HStack>
+                    <Text fontWeight={'bold'}>RuntimeMinutes:</Text>
+                    <Text>{title.runtimeMinutes ?? '-'}</Text>
+                  </HStack>
+                  <HStack>
+                    <Text fontWeight={'bold'}>Genres:</Text>
+                    <Text>{title.genresGenre?.map((genre) => genre?.genre).join(', ') ?? '-'}</Text>
+                  </HStack>
+                </Box>
+              ))}
           </Card.Body>
           <Card.Footer>
             <HStack justify={'end'}>
