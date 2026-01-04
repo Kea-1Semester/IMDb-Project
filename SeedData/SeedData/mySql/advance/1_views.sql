1ce8b80d0dbc6417a70deb06d9c553d5ddc9d84a
Shero Shvan <103439939+Rasho-dk@users.noreply.github.com>
2026-01-04 13:08:54 +0100

    fix: update MySQL run script to execute dump and create stored proced… (#129)

--- SeedData/SeedData/mySql/advance/1_views.sql
+++ SeedData/SeedData/mySql/advance/1_views.sql
@@ -1,14 +1,15 @@
-######################VIEWS##########################
-############################################################
+USE imdb;
 -- name: movie_rating_summary
 CREATE OR REPLACE VIEW movie_rating_summary AS
 SELECT t.title_id,
-       t.primary_title,
-       t.original_title,
-       t.start_year,
-       r.average_rating
+    t.primary_title,
+    t.original_title,
+    t.start_year,
+    r.average_rating
 FROM Titles t
-         JOIN Ratings r ON t.title_id = r.title_id;
+        JOIN Ratings r ON t.title_id = r.title_id;
+
+
 
 CREATE OR REPLACE VIEW titles_view AS
 SELECT title_id,
